1c8a990812e80480dae661eaed3937e62830bd88
boris-ny <b.nyilindek@alustudent.com>
2024-05-26 21:13:02 +0200

    chore: Refactor BasicAuth class for improved readability and error handling

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -57,41 +57,25 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
             self,
             user_email: str,
-            user_pwd: str
-    ) -> TypeVar('User'):
-        """user object from credentials"""
-        if user_email is None or type(user_email) is not str:
-            return None
-        if user_pwd is None or type(user_pwd) is not str:
-            return None
-
-        users = User.search({'email': user_email})
-
-        if not users:
-            return None
-        if users is None or len(users) == 0:
-            return None
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
+            user_pwd: str) -> TypeVar('User'):
+        """Retrieves a user based on the user's authentication credentials.
+        """
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """current user"""
-        header = self.authorization_header(request)
-        if header is None:
-            return None
-        b64 = self.extract_base64_authorization_header(header)
-        if b64 is None:
-            return None
-        decoded = self.decode_base64_authorization_header(
-            b64)
-        if decoded is None:
-            return None
-        user_info = self.extract_user_credentials(
-            decoded)
-        if user_info is None:
-            return None
-        email, pwd = user_info
-        user = self.user_object_from_credentials(email, pwd)
-        return user
+        """Retrieves the user from a request.
+        """
+        auth_header = self.authorization_header(request)
+        b64_auth_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(b64_auth_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
