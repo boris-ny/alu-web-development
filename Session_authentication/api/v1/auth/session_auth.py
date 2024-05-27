3229006ea55d0a79190613c358aad424b3b05545
boris-ny <b.nyilindek@alustudent.com>
2024-05-27 16:29:02 +0200

    Initializa Session auth

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env/ python3
+from uuid import uuid4
+from flask import request
+from .auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """Session authentication class.
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user"""
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
